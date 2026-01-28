1ac77512ce2c61498b3cebc7775c632a256d2e89
hemanth415 <hemanth.vs12@gmail.com>
2026-01-27 21:00:01 -0600

    Langchain ootb llm structured output for parsing

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0).bind(stop=None)
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(
@@ -29,12 +29,10 @@ agent = create_react_agent(
     tools=tools,
     prompt=react_prompt_with_format_instructions,
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, max_iterations=1, max_execution_time=120
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
     )
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
-
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     result = chain.invoke(
